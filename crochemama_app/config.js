82cd599071263ad1d7c09e6fb171acd10c399e1f
nihalsslab <nihalsslab@gmail.com>
2025-12-25 16:27:57 +0530

    Add files via upload

---
+++ crochemama_app/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby0Czy8y5NSkGWuxgk6B5Jc3RSpQBbi4JWz1fEdyD-fW7h6F1w49eNejXlQUgQ9xvSNAw/exec'
+};
+
+// Global shorthand for backward compatibility with existing code
+const SCRIPT_URL = CONFIG.SCRIPT_URL;
